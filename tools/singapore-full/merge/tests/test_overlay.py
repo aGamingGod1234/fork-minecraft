62f5d340669808ef3dfdd0f4270bcdddf4e25380
Lucas <aGamingGod1234@users.noreply.github.com>
2026-09-13 18:13:15 +0800

    Cover all emitted full-cube facade minerals and metals

--- tools/singapore-full/merge/tests/test_overlay.py
+++ tools/singapore-full/merge/tests/test_overlay.py
@@ -120,14 +120,16 @@ class OverlayTests(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "unsupported block"):
             self.render([self.row(0, 0, 1, 2, "unregistered:fake_material")])
 
-    def test_cbd_iron_structure_is_solid_in_all_heightmaps(self):
-        _, chunks, _ = self.render([self.row(0, 0, 1, 12, "minecraft:iron_block")], bounds=(0, 0, 16, 16))
-        self.assertEqual(block_at(chunks, 0, 11, 0), "minecraft:iron_block")
-        self.assertEqual(block_at(chunks, 0, 12, 0), "minecraft:air")
+    def test_emitted_facade_minerals_and_metals_are_solid_in_all_heightmaps(self):
+        names = ["iron_block", "calcite", "copper_block", "polished_deepslate", "polished_granite"]
+        _, chunks, _ = self.render([self.row(x, 0, 1, 12, "minecraft:" + name) for x, name in enumerate(names)], bounds=(0, 0, 16, 16))
         maps = chunks[(0, 0)].root.value["Heightmaps"].value
         self.assertEqual(len(maps), 4)
-        for heightmap in maps.values():
-            self.assertEqual(heightmap.value[0] & 511, 12 - MIN_Y)
+        for x, name in enumerate(names):
+            self.assertEqual(block_at(chunks, x, 11, 0), "minecraft:" + name)
+            self.assertEqual(block_at(chunks, x, 12, 0), "minecraft:air")
+            for heightmap in maps.values():
+                self.assertEqual((heightmap.value[0] >> (x * 9)) & 511, 12 - MIN_Y)
 
     def test_job_lease_scope_and_expiry(self):
         now = datetime.now(timezone.utc)
